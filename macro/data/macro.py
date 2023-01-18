c1d4c64571990710e3cd24e3dd7e847ce9f84e87
YooInseo <inseo0121@gmail.com>
2023-01-19 06:12:33 +0900

    feat: System Update

--- macro/data/macro.py
+++ macro/data/macro.py
@@ -11,6 +11,9 @@ from event.KeyEvent import KeyEvent
 from pynput.keyboard import Listener, Key
 
 class Macro:
+    def __init__(self, info):
+        self.info = info
+
     def startRecord(list, self):    
         # print("Current date:",datetime.utcnow())
         # date = datetime.utcnow() 
@@ -35,43 +38,12 @@ class Macro:
 
     def playRecord(self):
         print("기록을 재생합니다.")
-
-
+    
     def getTime():
-        # print("Current date:",datetime.utcnow())
         date = datetime.utcnow()  
-        # print("Number of days since epoch:",date)
+ 
         seconds =(date.total_seconds())
         milliseconds = round(seconds*1000)
-        # print("Milliseconds since epoch:",milliseconds)
 
-        # self.thread = threading.Thread( target=self.start)
-        # self.thread.start() # 스레드 시작
-
-
-
-
-    # data = list(); #포지션 값을 담을 리스트 
-    # start = False
-    # nowPosition = 0
-    # def __init__(self):
-    #     self.thread = threading.Thread( target=self.start)
-    #     self.thread.start() # 스레드 시작
-    
-    # def startRecord(self, start):
-    #     if(self.nowPosition == 0):
-    #         self.nowPosition = pyautogui.position()
-    #     while(start):
-    #         if(self.nowPosition != pyautogui.position()): # 기존의 값과 다르면 
-    #             self.data.append(pyautogui.position())
-    #             self.nowPosition = pyautogui.position()
-    #             print(self.data)
-    #             time.sleep(0.5)
- 
-
-    # def stop(self):
-    #     self.start = False
-    #     self.thread.stop()
-
-    # def record(self):
-    #     self.start = True 
+    def getInfo(self):
+        return self.info
--- macro/gui/gui.py
+++ macro/gui/gui.py
@@ -5,18 +5,20 @@ from PyQt5.QtCore import pyqtSlot
 from data.macro import Macro
 import pyautogui
  
-class window():
+from data.data import data
 
+class window():
    def __init__(self):
       app = QApplication(sys.argv)
       widget = QWidget()
-      macro = Macro()
-      
-      #키값 리스트 생성
-      keyList = ['T', 'n', 'r', ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', 'accept', 'add', 'alt', 'altleft', 'altright', 'apps', 'backspace', 'browserback', 'browserfavorites', 'browserforward', 'browserhome', 'browserrefresh', 'browsersearch', 'browserstop', 'capslock', 'clear', 'convert', 'ctrl', 'ctrlleft', 'ctrlright', 'decimal', 'del', 'delete', 'divide', 'down', 'end', 'enter', 'esc', 'escape', 'execute', 'f1', 'f10', 'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f2', 'f20', 'f21', 'f22', 'f23', 'f24', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'final', 'fn', 'hanguel', 'hangul', 'hanja', 'help', 'home', 'insert', 'junja', 'kana', 'kanji', 'launchapp1', 'launchapp2', 'launchmail', 'launchmediaselect', 'left', 'modechange', 'multiply', 'nexttrack', 'nonconvert', 'num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6', 'num7', 'num8', 'num9', 'numlock', 'pagedown', 'pageup', 'pause', 'pgdn', 'pgup', 'playpause', 'prevtrack', 'print', 'printscreen', 'prntscrn', 'prtsc', 'prtscr', 'return', 'right', 'scrolllock', 'select', 'separator', 'shift', 'shiftleft', 'shiftright', 'sleep', 'space', 'stop', 'subtract', 'tab', 'up', 'volumedown', 'volumemute', 'volumeup', 'win', 'winleft', 'winright', 'yen', 'command', 'option', 'optionleft', 'optionright']
-      
-      cb = QComboBox(widget)
-      
+
+      info = data()
+
+      macro = Macro(info)
+
+      keyList = data.keyList
+
+      cb = QComboBox(widget)  
 
       for a in keyList: #키값 리스트를 콤보박스에 추가
          cb.addItem(a)
@@ -35,8 +37,8 @@ class window():
  
       btnRecord = QPushButton("기록",widget)	# 버튼 텍스트
       # btnRun.move(20, 20)	# 버튼 위치
-   
-      btnRecord.clicked.connect(lambda: Macro.startRecord(list, macro))	# 버튼 클릭 시 이벤트 연결
+ 
+      btnRecord.clicked.connect(lambda: macro.startRecord(list))	# 버튼 클릭 시 이벤트 연결
 
       widget.setGeometry(1000,450,320,200)
       widget.setWindowTitle("Macro Recorder")
@@ -44,7 +46,7 @@ class window():
 
 
       sys.exit(app.exec_())
-
+ 
    def key():
       print("key")
 
