f65d02fda6dbd8aff70168133f7f0a9bc1e4f2e9
SkyExcel <inseo0121@gmail.com>
2023-01-22 12:05:13 +0900

    feat: System Update

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
 from PyQt5       import QtWidgets, QtGui, QtCore
+from PyQt5.QtCore import Qt
+
 from pynput import keyboard
 from PyQt5.QtCore import QThread
 from datetime import datetime
 import time
+from pynput import mouse
+
 import pyautogui
 
 class data:
@@ -11,6 +15,8 @@ class data:
     playing = False
     isRecording = False
     record = set()
+    index = 0
+    info = dict()
 
 class Macro(QThread):
 
@@ -23,14 +29,10 @@ class Macro(QThread):
         self.settingButton = Widget.settingButton
          
     def run(self):
-        index = 0
+         
         while(True):
             if(data.isRecording):
-                now = datetime.now()
-                x = pyautogui.position()[0]
-                y = pyautogui.position()[1]
-
-                it = QtGui.QStandardItem("x= (" +str(x) +")" + " y= (" +str(y) +")")
+                it = QtGui.QStandardItem("마우스 움직임")
                 self.entry.appendRow(it)
                 
                 self.loadButton.setDisabled(True)
@@ -38,9 +40,11 @@ class Macro(QThread):
                 self.startButton.setDisabled(True)
                 self.settingButton.setDisabled(True)
                 
-                data.record.add(pyautogui.position())
-           
-
+                data.record.add(pyautogui.position()) #TODO position 값은 Key로 지정, ClickType은 Value로 지정하여 불러온다.
+                # data.info.update({pyautogui.position(): ""})
+            
+                # if(pyautogui.rightClick()):
+                #     print("left click")
                 time.sleep(0.1)
             elif data.playing is not True:
                 self.loadButton.setDisabled(False)
@@ -50,13 +54,13 @@ class Macro(QThread):
                 
             else:
                 length = len(data.record)
-                if (length - 1) != index:
-                    index += 1
-                    position = list(data.record)[index]
+                if (length - 1) != data.index:
+                    data.index += 1
+                    position = list(data.record)[data.index]
                     pyautogui.moveTo(position[0], position[1])
                     time.sleep(0.1)
                 else:
-                    index=0
+                    data.index=0
                     break
                     
 class Listner(QThread):
@@ -64,7 +68,10 @@ class Listner(QThread):
         super().__init__()  
   
     def on_press(self,key):
-        try:
+        try: 
+            #TODO 특수 키를 입력하기 위해선 except를 사용 해야 하는데, 해당 방법을 사용 할 경우, 강제적으로 일반 키보드도 넣어줘야한다.
+            #리펙토링 필요함.
+
             if data.startKey is not any: #startKey 값이 any 타입이 아닐 경우 
                 if (str(key).replace("Key.","")) == data.startKey: 
                     print(f'알파벳 \'{key.char}\' 눌림ㅎㅎ' )
@@ -78,17 +85,18 @@ class Listner(QThread):
                         data.isRecording = True
                     else:
                         data.isRecording = False
-                     
+            
     def on_release(self,key):
         if key == keyboard.Key.esc:
             # esc 키에서 풀림
             return False
-
     def run(self):
         with keyboard.Listener(on_press=self.on_press, on_release=self.on_release) as listener:
          listener.join()
+        
 
 class Widget(QtWidgets.QWidget): #TODO 저장, 초기화 버튼 만들기
+ 
     def __init__(self):
         super().__init__()
         lay = QtWidgets.QVBoxLayout(self)
@@ -101,13 +109,25 @@ class Widget(QtWidgets.QWidget): #TODO 저장, 초기화 버튼 만들기
         self.recordButton = QtWidgets.QPushButton()
 
         self.combo_box = QtWidgets.QComboBox()
-  
+
         self.combo_box.setGeometry(0, 0, 300, 100)
         
         self.combo_box.currentTextChanged.connect(self.changeKey)
 
         names = [member.name for member in keyboard.Key]
         self.combo_box.addItems(names)
+
+        self.hlay = QtWidgets.QHBoxLayout()
+
+        lay.addLayout(self.hlay)
+
+        self.clearButton = QtWidgets.QPushButton("초기화")
+        self.clearButton.setToolTip("모든 메크로 기록을 지웁니다.")  
+
+        self.saveButton = QtWidgets.QPushButton("저장")
+        self.saveButton.setToolTip("메크로를 새로 만듭니다.")  
+
+        self.hlay.addWidget(self.clearButton)
 
         self.newButton = QtWidgets.QPushButton("새로 만들기")
         self.newButton.setToolTip("메크로를 새로 만듭니다.")        
@@ -124,11 +144,14 @@ class Widget(QtWidgets.QWidget): #TODO 저장, 초기화 버튼 만들기
         lay.addWidget(self.listView)
 
         self.hlay = QtWidgets.QHBoxLayout()
+        
         lay.addLayout(self.hlay)
+
         self.hlay.addWidget(self.newButton)
         self.hlay.addWidget(self.loadButton)
         self.hlay.addWidget(self.startButton)
         self.hlay.addWidget(self.settingButton)
+
         self.entry = QtGui.QStandardItemModel()
         self.listView.setModel(self.entry)
 
